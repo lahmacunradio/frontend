8a846291807e4e7243a1cbe9a6b5f3498fa23fe1
Gyorgy Gabor <gyorgyg@gmail.com>
2021-06-06 19:27:15 +0200

    feat: rename SearchInput component. Implement TextInput component

---
+++ components/AutoCompleteSearch.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <TextInput v-model='value' />
+    <ul class='suggestions'>
+      <li
+        v-for='(suggestion, i) in suggestions'
+        :key='suggestionAttribute ? suggestion.id : suggestion + i'
+        class='suggestion'
+      >
+        {{ suggestionAttribute ? suggestion[suggestionAttribute] : suggestion }}
+      </li>
+    </ul>
+    <p>{{value}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      required: true,
+      type: Array
+    },
+    suggestionAttribute: {
+      required: false,
+      type: String,
+      default: null
+    }
+  },
+  data () {
+    return {
+      value: ''
+    }
+  },
+  computed: {
+    suggestions () {
+      return [{ id: '1', name: 'name1' }, { id: '2', name: 'name2' }]
+    }
+  }
+}
+</script>
