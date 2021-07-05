3d6c200bfb363b509141634609eb4e1b0aadc851
baxgas <baxgas@gmail.com>
2021-07-05 08:30:38 +0200

    #75 add progress bar and time display

--- components/ArcsiPlayer.vue
+++ components/ArcsiPlayer.vue
@@ -6,10 +6,6 @@
     </div>
     <div v-else>
       <div>
-        <span>Total duration: {{ duration }} seconds</span>
-        <span>Progress: {{ (progress * 100) }}%</span>
-      </div>
-      <div>
         <button @click="togglePlayback">
           <span v-if="playing">
             <i class="fa fa-pause" aria-hidden="true" />
@@ -22,18 +18,47 @@
           <i class="fa fa-stop" aria-hidden="true" />
         </button>
       </div>
+      <div id="myProgress" class="my-2">
+        <div id="myBar" :style="{width: Math.round(progress * 100) + '%'}" />
+      </div>
+      <div>
+        <span>Total duration: {{ currentDuration }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import VueHowler from 'vue-howler'
+import { format, addSeconds } from 'date-fns'
 
 export default {
   mixins: [VueHowler],
   data () {
     return {
     }
+  },
+  computed: {
+    currentDuration () {
+      const helperDate = addSeconds(new Date(0), this.duration)
+      return format(helperDate, 'mm:ss')
+    }
+  },
+  methods: {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "/assets/css/variables";
+ #myProgress {
+  width: 100%;
+  background-color: white;
+}
+
+#myBar {
+  width: 1%;
+  height: 1rem;
+  background-color: $black-color;
+}
+</style>
