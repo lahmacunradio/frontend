68cf8578aa53d4292491a4699f14b2cfbda70f7c
baxgas <baxgas@gmail.com>
2021-08-16 15:30:40 +0200

    #88 implement Slider for Latest Shows

--- app/components/arcsi/Latest.vue
+++ app/components/arcsi/Latest.vue
@@ -2,19 +2,68 @@
   <div>
     <h3>Arcsi's Latest</h3>
     <div class="container py-8">
-      <div v-for="(episode, i) in arcsiEpisodesListSortedLatest" :key="i">
-        {{ episode.name }}
+      <div v-swiper="swiperOption" class="relative" :loadtheme="false">
+        <div class="swiper-wrapper">
+          <div v-for="(episode, i) in arcsiEpisodesListSortedLatest" :key="i" class="swiper-slide">
+            <div class="latest-arcsi-blokk">
+              <div class="mb-4 arcsi-img">
+                <img class="block" :src="episode.image_url">
+              </div>
+              <h5>{{ episode.name }}</h5>
+            </div>
+          </div>
+        </div>
+        <div slot="button-prev" class="swiper-button-prev" />
+        <div slot="button-next" class="swiper-button-next" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { directive } from 'vue-awesome-swiper'
+
 export default {
+  directives: {
+    swiper: directive
+  },
   data () {
     return {
       startIndex: 0,
-      numberOfEpisodes: 9
+      numberOfEpisodes: 9,
+      swiperOption: {
+        slidesPerView: 3,
+        spaceBetween: -10,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        breakpoints: {
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 10
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 10
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 10
+          },
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          }
+        }
+      }
     }
   },
   computed: {
@@ -44,4 +93,18 @@ h3 {
     font-weight: 400;
     text-align: center;
 }
+
+.latest-arcsi-blokk {
+  .arcsi-img {
+    height: 300px;
+    width: 100%;
+    overflow: hidden;
+    img {
+      object-fit: cover;
+      min-height: 100%;
+      min-width: 100%;
+    }
+  }
+}
+
 </style>
---
+++ app/plugins/vue-awesome-swiper.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+import '@/assets/css/swiper.min.css'
+// import custom style
+import '@/assets/css/customswiper.scss'
+
+Vue.use(VueAwesomeSwiper)
