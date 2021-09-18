3ef31931e630ceef584caa78042a184540443db4
baxgas <baxgas@gmail.com>
2021-09-18 11:34:49 +0200

    #92 news pagination start

--- app/pages/news/index.vue
+++ app/pages/news/index.vue
@@ -1,32 +1,93 @@
 <template>
   <div class="container">
-    <h1 class="mb-8">News</h1>
-    <article class="grid gap-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+    <header class="flex flex-row items-center justify-between">
+      <h1 class="mb-8">
+        News
+      </h1>
+      <div>
+        <select id="year" name="">
+          <option disabled selected value>
+            Year
+          </option>
+          <option value="2021" name="2021">
+            2021
+          </option>
+          <option value="2020" name="2020">
+            2020
+          </option>
+        </select>
+        <select id="Tag" name="">
+          <option disabled selected value>
+            Tag
+          </option>
+          <option value="music" name="music">
+            Music
+          </option>
+          <option value="event" name="event">
+            Event
+          </option>
+          <option value="merch" name="merch">
+            Merch
+          </option>
+        </select>
+      </div>
+    </header>
+    <article v-if="newsFilteredList.length" class="grid gap-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+      <div v-for="news in newsFilteredList" :key="news.id">
+        <NewsBlock :news="news" />
+      </div>
+    </article>
+    <article v-else class="grid gap-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
       <div v-for="news in sortNews" :key="news.id">
         <NewsBlock :news="news" />
       </div>
     </article>
+    <footer class="flex flex-row justify-center py-4 align-middle news-pagination">
+      <div>First |</div>
+      <div> Previous |</div>
+      <div>
+        <a href="#" @click.prevent="fetchNewsPagination(1)">Next</a>
+        |
+      </div>
+      <div> Last</div>
+    </footer>
   </div>
 </template>
 
 <script>
+import { newsBaseURL } from '~/constants'
 
 export default {
   components: {},
-  data() {
-    return {}
+  data () {
+    return {
+      newsFilteredList: {},
+      numberOfItems: 12,
+      startOffset: 1
+    }
   },
-  head() {
+  head () {
     return {
       title: 'Lahmacun News'
     }
   },
   computed: {
-    newsList() {
+    newsList () {
       return this.$store.state.newsList
     },
-    sortNews() {
+    sortNews () {
       return [...this.newsList].sort((a, b) => a.date - b.date)
+    }
+  },
+  methods: {
+    async fetchNewsPagination (offset) {
+      this.newsFilteredList = await fetch(`${newsBaseURL}&per_page=${this.numberOfItems}&offset=${this.numberOfItems * this.startOffset}`)
+        .then(res => res.json())
+      this.startOffset = this.startOffset + offset
+    },
+    async fetchNewsYear (year) {
+      this.newsFilteredList = await fetch(`${newsBaseURL}&tags=${year}`)
+        .then(res => res.json())
     }
   }
 }
