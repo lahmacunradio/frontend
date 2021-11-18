aa52c5d8cee509f46716f710718910d73e9ff44c
baxgas <baxgas@gmail.com>
2021-11-18 16:43:18 +0100

    #149 Schedule add arcsi blocks

--- app/components/schedule/Fullitem.vue
+++ app/components/schedule/Fullitem.vue
@@ -12,7 +12,7 @@
           On Air
         </div>
       </div>
-      <div class="schedule-infos">
+      <div class="flex justify-between w-full schedule-infos">
         <div v-if="showAirCheck(show.name)" class="flex">
           <div class="mr-4 onair-image">
             <img :src="onAirImage" :alt="show.name">
@@ -22,9 +22,24 @@
               <b>{{ show.name }} {{ showAirCheck(show.name) && ' | ' + streamEpisodeTitle }} </b>
             </NuxtLink>
             <div class="mb-2 text-sm onair-meta">
-              {{ showFrequency(show.frequency, show.week) }} | 
+              {{ showFrequency(show.frequency, show.week) }} |
               Language: <span v-sanitize.nothing="getLanguageGraph(show.language)" class="language" />
             </div>
+            <div class="text-sm description">
+              {{ onAirDescription }}
+            </div>
+          </div>
+        </div>
+        <div v-else-if="opened" class="flex">
+          <div class="mr-4 onair-image">
+            <NuxtLink :to="latestEpisodeLink" class="block mb-2">
+              <img :src="latestEpisodeImage" :alt="show.name">
+            </NuxtLink>
+          </div>
+          <div class="onair-infos">
+            <NuxtLink :to="latestEpisodeLink" class="block mb-2">
+              <b>{{ latestEpisodeTitle }} </b>
+            </NuxtLink>
             <div class="text-sm description">
               {{ onAirDescription }}
             </div>
@@ -35,12 +50,21 @@
             <b>{{ show.name }}</b>
           </NuxtLink>
         </div>
+        <div v-if="!showAirCheck(show.name)">
+          <a href="#" @click.prevent="opened = !opened">
+            <i v-if="opened" class="fa fa-chevron-up" aria-hidden="true" />
+            <i v-else class="fa fa-chevron-down" aria-hidden="true" />
+          </a>
+        </div>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
 <script>
+import { mediaServerURL } from '~/constants'
+
 export default {
   props: {
     show: {
@@ -54,6 +78,7 @@ export default {
   },
   data () {
     return {
+      opened: false
     }
   },
   computed: {
@@ -61,7 +86,7 @@ export default {
       return ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
     },
     streamShowTitle () {
-      if (!this.nowPlaying) {
+      if (!this.nowPlaying.now_playing) {
         return false
       } else if (this.nowPlaying?.live?.is_live) {
         return this.nowPlaying?.live?.streamer_name
@@ -70,7 +95,7 @@ export default {
       }
     },
     streamEpisodeTitle () {
-      if (!this.nowPlaying) {
+      if (!this.nowPlaying.now_playing) {
         return false
       } else if (this.nowPlaying?.live?.is_live) {
         return this.nowPlaying?.live?.song?.title
@@ -79,19 +104,44 @@ export default {
       }
     },
     onAirImage () {
-      if (!this.nowPlaying) {
+      if (!this.nowPlaying.now_playing) {
         return false
       }
       const streamImage = this.nowPlaying.now_playing?.song?.art
       return this.showAirCheck(this.show.name) ? streamImage : this.show.cover_image_url
     },
+    latestEpisodeImage () {
+      if (!this.show.items) {
+        return false
+      }
+      const getLastIndex = this.show.items.length - 1
+      const episodeImageFromArcsi = this.show.items?.[getLastIndex]?.image_url
+      return episodeImageFromArcsi ? `${mediaServerURL}/${this.show.archive_lahmastore_base_url}/${episodeImageFromArcsi}` : this.show.cover_image_url
+    },
     onAirDescription () {
-      if (!this.nowPlaying && this.show) {
+      if (!this.nowPlaying.now_playing && this.show.items) {
         return false
       }
       const getLastIndex = this.show.items.length - 1
       const descriptionFromArcsi = this.show.items?.[getLastIndex]?.description
       return descriptionFromArcsi || this.show.description
+    },
+    latestEpisodeTitle () {
+      if (!this.show.items) {
+        return this.show.name
+      }
+      const getLastIndex = this.show.items.length - 1
+      const episodeTitleFromArcsi = this.show.items?.[getLastIndex]?.name
+      return episodeTitleFromArcsi ? `${this.show.name} - ${episodeTitleFromArcsi}` : this.show.name
+    },
+    latestEpisodeLink () {
+      const baseLink = '/shows/' + this.show.archive_lahmastore_base_url
+      if (!this.show.items) {
+        return baseLink
+      }
+      const getLastIndex = this.show.items.length - 1
+      const episodeIdFromArcsi = this.show.items?.[getLastIndex]?.id
+      return episodeIdFromArcsi ? `/shows/${this.show.archive_lahmastore_base_url}/${episodeIdFromArcsi}` : baseLink
     }
   },
   methods: {
