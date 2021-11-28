805c04a5fd9b5dc6b2a776bce0d825da4c5bb71e
Gyorgy Gabor <gyorgyg@gmail.com>
2021-11-28 01:50:42 +0100

    feat(itemBlock): render item

--- app/components/item/Block.vue
+++ app/components/item/Block.vue
@@ -1,28 +1,27 @@
 <template>
   <div class="mb-2">
-<!--    <NuxtLink :to="'/news/' + news.slug">-->
+    <NuxtLink :to="item.url">
       <div class="item-image">
         <img
-          :src="itemsImage"
-          :srcset="`${itemsImageSmall} 480w`"
+          :src="image"
+          :srcset="`${smallImage} 480w`"
           sizes="(max-width: 640px) 480px,
             800px"
-          :alt="item.title.rendered"
+          :alt="item.title"
           class="mb-4"
         >
       </div>
       <h5 class="py-4">
-        {{ htmlDecoder(item.title.rendered) }}
+        {{ item.title }}
       </h5>
-<!--    </NuxtLink>-->
+    </NuxtLink>
     <div class="item-text">
-      <div v-sanitize="truncatedItem" />
+      <div v-sanitize="item.description" />
     </div>
   </div>
 </template>
 
 <script>
-import { contentApiURL } from '~/constants'
 
 export default {
   components: {
@@ -35,26 +34,19 @@ export default {
   },
   data () {
     return {
-      itemImage: require('@/assets/img/lahmacun-logo-dummy.png'),
-      itemImageSmall: require('@/assets/img/lahmacun-logo-dummy.png')
+      image: require('@/assets/img/lahmacun-logo-dummy.png'),
+      smallImage: require('@/assets/img/lahmacun-logo-dummy.png')
     }
   },
   computed: {
-    truncatedNews () {
-      return this.truncate(this.item.excerpt.rendered, 150)
-    }
   },
   mounted () {
-    this.loadNewsImages(this.item.featured_media)
+    this.loadNewsImages()
   },
   methods: {
-    async loadNewsImages (newsId) {
-      console.log('DDDDDDD', this.item)
-      debugger
-      // const adress = `${contentApiURL}/media/${newsId}`
-      // const responseNews = await this.$axios.get(adress)
-      // this.newsImage = responseNews.data?.media_details?.sizes?.large?.source_url || responseNews.data?.source_url || this.newsImage
-      // this.newsImageSmall = responseNews.data?.media_details?.sizes?.medium_large?.source_url || responseNews.data?.source_url || this.newsImage
+    async loadNewsImages() {
+      this.image = this.item.image.large || this.image
+      this.smallImage = this.item.image.small || this.smallImage
     }
   }
 }
