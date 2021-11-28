14bc0d2be9375fad9cb2737b6544439982246b50
Gyorgy Gabor <gyorgyg@gmail.com>
2021-11-28 18:32:59 +0100

    feat(Pagination): create component

---
+++ app/components/Pagination.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div v-if="totalCount > itemsCount && !isLoading" class="p-4 text-center load-more-container">
+      <a href="#" @click.prevent="onClick">
+        <div class="load-more">
+            LOAD MORE
+        </div>
+      </a>
+    </div>
+    <div v-if="isLoading" class="flex flex-col items-center justify-center py-4">
+      <img src="@/assets/img/preloader.svg" class="h-8 mb-2">
+      <p>Loading...</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    itemsCount: {
+      type: Number,
+      required: true
+    },
+    isLoading: {
+      type: Boolean,
+      required: true
+    },
+    totalCount: {
+      type: Number,
+      required: true
+    },
+  },
+  methods: {
+    async onClick() {
+      this.$emit('click');
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+.load-more-container {
+  display: flex;
+  justify-content: center;
+}
+.load-more {
+  width: 184.81px;
+  height: 88.03px;
+  background: #FFFFFF;
+  transform: matrix(0.98, -0.2, 0.22, 0.98, 0, 0);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px / 50px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  transform: rotate(-11.5deg);
+}
+</style>
