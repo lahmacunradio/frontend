aa066e0f53136eab62d9dd9e0b385619a37900d9
baxgas <baxgas@gmail.com>
2021-12-15 16:05:15 +0100

    #197 add head meta

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -14,9 +14,10 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Budapest online radio and more' },
+      { hid: 'description', name: 'description', content: 'Lahmacun.hu is an online music & more radio from Budapest since 2018' },
+      { hid: 'keywords', name: 'keywords', content: 'budapest online radio' },
       { hid: 'og:title', name: 'og:title', content: 'Lahmacun Radio' },
-      { hid: 'og:description', name: 'og:description', content: 'Budapest online radio and more' },
+      { hid: 'og:description', name: 'og:description', content: 'Lahmacun.hu is an online music & more radio from Budapest since 2018' },
       { hid: 'og:image', name: 'og:image', content: '/lahmacun-logo-share.jpg' }
     ],
     link: [
