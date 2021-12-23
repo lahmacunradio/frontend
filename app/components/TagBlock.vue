bea46b99ebc03c1db0d21f4eb1aaef10ef5b907a
Gyorgy Gabor <gyorgyg@gmail.com>
2021-12-23 19:08:15 +0100

    feat(tags): add tagList and tagBlock

---
+++ app/components/TagBlock.vue
@@ -0,0 +1,32 @@
+<template>
+  <NuxtLink :to="link" class="tag-item">
+    {{ tag.name }}
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: {
+      type: Object,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tag-item {
+  background-color: #FFFFFF;
+  display: inline-block;
+  padding: 0.25rem 0.75rem;
+}
+
+.tag-item:hover {
+  color: $lahma-pink;
+}
+</style>
---
+++ app/components/TagList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tag-list">
+    <TagBlock
+      v-for="(tag, index) in tags" :key="`${tag.id}-${index}`"
+      :tag="tag"
+      :link="`/news/tags/${tag.slug}`"
+      class="tag"
+    />
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    tags: {
+      type: Array,
+      required: true
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tag-list {
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  width: 100%;
+}
+.tag {
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+</style>
