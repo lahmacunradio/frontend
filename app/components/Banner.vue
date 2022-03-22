6c631be2a295dc12fc4c00072d5f21a43721e8b2
Peti <bokorpet@gmail.com>
2022-03-22 11:46:10 +0100

    Initial Vuetify banner setup
    #250
    Issues/todos: style overrides via Banner component (not via global app styling), button logic (methods), text from CMS

---
+++ app/components/Banner.vue
@@ -0,0 +1,38 @@
+<template>
+        <v-app>
+        <v-banner
+          color="white">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+    Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+    <template v-slot:actions>
+      <v-btn
+        text
+        color="primary"
+      >
+        Dismiss
+      </v-btn>
+      <v-btn
+        text
+        color="primary"
+      >
+        Retry
+      </v-btn>
+    </template>
+  </v-banner>
+  </v-app>
+</template>
+  
+<script>
+export default {
+    name: 'Banner'
+};
+</script>
+
+<style lang="scss" scoped>
+    @import '~vuetify/src/components/VStepper/_variables.scss';
+
+    .v-banner{
+        position:fixed;
+        bottom:50px;        
+    }
+</style>
