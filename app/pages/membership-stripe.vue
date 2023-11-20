a70917dab657118645a42761f57078b4b017b49a
baxgas <baxgas@gmail.com>
2023-11-20 21:40:00 +0100

    add selector

--- app/pages/membership-stripe.vue
+++ app/pages/membership-stripe.vue
@@ -1,20 +1,28 @@
 <template>
   <div>
-    <SubTitle title="Lahmacun Stripe" :maintitle="true" />
+    <SubTitle title="Lahmacun show membership" :maintitle="true" />
     <div class="container my-8">
-      <div class="product">
-        <img src="@/assets/img/lahma_logo_1.svg" alt="Lahmacun Radio" class="w-24 logo">
-        <div class="description">
-          <h3>Lahmacun show membership</h3>
-          <p>DAILY RECURRING PAYMENT</p>
-          <h5>0.1 EUR</h5>
-        </div>
+      <div class="product mb-4">
+        <p>DAILY RECURRING PAYMENT</p>
+        <h5>0.1 EUR</h5>
       </div>
 
       <form action="https://cms.lahmacun.hu/wp-json/stripe/return_checkout_session_recurring_membership" method="GET">
         <div>
           <label for="show_name">Show name:</label>
-          <input name="show_name" id="show_name" value="MMN radio" />
+          <div class="relative max-w-md bg-white rounded-sm">
+            <select v-model="show_name" class="show-select" :class="{ showSelected: show_name.length > 1 }"
+              @change="selectShow">
+              <option disabled selected value="">Please select a show</option>
+              <option v-for="(show) in arcsiShowsList" :value="show.archive_lahmastore_base_url" :key="index">
+                {{ show.name }}
+              </option>
+            </select>
+            <div class="absolute right-2 top-2 z-0">
+              <i class="fa fa-chevron-down"/>
+            </div>
+          </div>
+
         </div>
         <button type="submit" id="checkout-button">Checkout</button>
       </form>
@@ -29,6 +37,7 @@ import { mapGetters } from 'vuex'
 export default {
   data() {
     return {
+      show_name: "",
     }
   },
   mounted() {
@@ -36,7 +45,7 @@ export default {
     stripeScript.setAttribute('src', 'https://js.stripe.com/v3/')
     document.head.appendChild(stripeScript)
   },
-  head () {
+  head() {
     return {
       title: 'Lahmacun Membership',
       meta: [
@@ -52,7 +61,7 @@ export default {
     ...mapGetters({
       allShows: 'returnArcsiShows'
     }),
-    arcsiShowsList () {
+    arcsiShowsList() {
       if (this.allShows) {
         return this.allShows.filter(show => (
           !(show.archive_lahmastore_base_url === 'off-air' || !show.active)
@@ -61,6 +70,29 @@ export default {
       return null
     },
   },
+  methods: {
+    selectShow(showname) {
+      this.show_name = showname.target.value
+    },
+  }
 
 }
 </script>
+
+<style scoped lang="scss">
+.show-select {
+  @apply block p-2 my-2 border w-full text-gray-500 z-10 relative;
+}
+
+.show-select:focus {
+  @apply outline-none;
+}
+
+.show-select.showSelected {
+  @apply text-black;
+}
+
+#checkout-button {
+  @apply bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-sm my-8;
+}
+</style>
